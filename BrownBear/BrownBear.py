fcb0a24dab172cacbff0cc05725d3cfa5b041168
Tim <tim.andes@gmail.com>
2020-10-01 16:52:28 -0400

    Added docstrings to BrownBear.py: Item(), Item.answer(), PluralItem(), main()

--- BrownBear/BrownBear.py
+++ BrownBear/BrownBear.py
@@ -13,6 +13,15 @@ items = [
 ]
 
 class Item():
+    """Simulates each item's name, question, and answer based on list position.
+
+    Note:
+        Any mention of 'item' is in reference to the current iteration's
+        animal or person(s) e.g. 'Brown Bear'.
+
+    Args:
+        name (str): Name of current item in the list, 'items'.
+    """
     def __init__(self, name: str):
         self.name = name
 
@@ -26,6 +35,15 @@ class Item():
         return f'{self.name}, {self.name}, what do you see?'
 
     def answer(self, view):
+        """Prints the current item's answer to question().
+
+        Args:
+            view (str): Name of what the current item sees.
+
+        Returns:
+            An f-string, dependent on whether number of animals/people in
+            the current item's view is one or more than one.
+        """
         if len(view) > 1:
             viewstr = ' a ' + ', a '.join(v.name for v in view[:-1])
             viewstr += f' and a {view[-1].name}'
@@ -37,6 +55,7 @@ class Item():
         return f'{self.subj_phrase()}{viewstr} looking at {self.obj_phrase()}.'
 
 class PluralItem(Item):
+    """Handles the plural Item 'Children' and its required pronouns."""
     def subj_phrase(self):
         return 'We see'
 
@@ -44,6 +63,7 @@ class PluralItem(Item):
         return 'us.  That\'s what we see'
 
 def main():
+    """Instantiates items and prints the determined question and answer."""
     items = [
         Item('Brown Bear'),
         Item('Red Bird'),
